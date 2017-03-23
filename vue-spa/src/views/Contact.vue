41bebe051a92c4e8a4e408b0769f51cdda2154c7
Jon Wynveen <jwynveen@mpirik.com>
2017-03-23 14:19:36 -0500

    contact form submission

--- vue-spa/src/views/Contact.vue
+++ vue-spa/src/views/Contact.vue
@@ -2,15 +2,35 @@
   <div>
     <h1>Contact</h1>
 
-    <pre v-if="isPost"><%= JSON.stringify(formData, null, 2) %></pre>
+    <pre v-if="isPost">
+      {
+        name: {{name}}
+      }
+    </pre>
 
-    <form v-else method="post">
+    <form v-else v-on:submit="onSubmit" method="post">
       <div class="form-group">
         <label class="control-label">Name</label>
-        <input type="text" name="name" />
+        <input type="text" name="name" v-model="name" />
       </div>
 
       <button type="submit" class="btn btn-primary">Submit</button>
     </form>
   </div>
 </template>
+
+<script>
+  export default {
+    name: 'contact',
+    data() {
+      return {
+        isPost: false,
+      };
+    },
+    methods: {
+      onSubmit: function (evt) {
+        this.isPost = true;
+      }
+    }
+  }
+</script>
