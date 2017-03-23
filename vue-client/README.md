7a2c6ffa6bef6a47081d3c4a829f5b01c2f0c087
Jon Wynveen <jwynveen@mpirik.com>
2017-03-23 15:52:34 -0500

    set port for express app

--- vue-client/README.md
+++ vue-client/README.md
@@ -7,4 +7,4 @@ $ npm install
 $ npm start
 ```
 
-Go to [http://localhost:3000](http://localhost:3000)
+Go to [http://localhost:3000](http://localhost:3001)
--- vue-client/app.js
+++ vue-client/app.js
@@ -43,4 +43,8 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
+app.listen(3001, function() {
+  console.log('Listening on port 3001')
+})
+
 module.exports = app;
