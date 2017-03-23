3b4fbc046706110b90305eb278149a23b5006c31
Jon Wynveen <jwynveen@mpirik.com>
2017-03-23 10:53:31 -0500

    Basic views for express app

--- vue-client/routes/index.js
+++ vue-client/routes/index.js
@@ -3,7 +3,29 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', {
+    title: 'Express',
+  });
+});
+
+// About
+router.get('/about', function(req, res, next) {
+  res.render('about');
+});
+
+// Contact
+router.get('/contact', function(req, res, next) {
+  const data = {
+    isPost: false,
+  };
+  res.render('contact', data);
+});
+router.post('/contact', function(req, res, next) {
+  const data = {
+    isPost: true,
+    formData: req.body,
+  }
+  res.render('contact', data);
 });
 
 module.exports = router;
